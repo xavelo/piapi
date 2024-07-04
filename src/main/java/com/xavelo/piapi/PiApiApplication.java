31281b60bc4c3152cc7a4e04f262b2bf2d68a922
xavelo <xavelo@gmail.com>
2024-07-04 15:41:00 +0200

    actuators

--- src/main/java/com/xavelo/piapi/PiApiApplication.java
+++ src/main/java/com/xavelo/piapi/PiApiApplication.java
@@ -2,14 +2,25 @@ package com.xavelo.piapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:git.properties")
 public class PiApiApplication {
 
+	private static ApplicationContext applicationContext;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PiApiApplication.class, args);
+		displayAllBeans();
+	}
+
+	public static void displayAllBeans() {
+		String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+		for(String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
