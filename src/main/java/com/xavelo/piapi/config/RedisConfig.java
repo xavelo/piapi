bb28cb3dc7b5bb0a49799fa78ca4c1c7c9a8135c
xavelo <xavelo@gmail.com>
2024-07-04 15:13:52 +0200

    Redis config

--- src/main/java/com/xavelo/piapi/config/RedisConfig.java
+++ src/main/java/com/xavelo/piapi/config/RedisConfig.java
@@ -1,18 +1,29 @@
 package com.xavelo.piapi.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Slf4j
 @Configuration
+@Profile("!test")
 public class RedisConfig {
-
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        try {
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+            redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+            redisTemplate.setConnectionFactory(connectionFactory);
+        } catch (Exception e) {
+            log.error("Error getting Redis Template connection ", e);
+        }
+        return redisTemplate;
     }
 
 }
