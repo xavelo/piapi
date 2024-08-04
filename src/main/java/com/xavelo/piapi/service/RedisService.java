783689a9a83b9e03403c559b50099e1b8fdd4f16
xavelo <xavelo@gmail.com>
2024-08-04 18:07:40 +0200

    RedisTemplate<String, Object>

--- src/main/java/com/xavelo/piapi/service/RedisService.java
+++ src/main/java/com/xavelo/piapi/service/RedisService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
