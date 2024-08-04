5de37f1b3132dc327572513322c88eb3255b63df
xavelo <xavelo@gmail.com>
2024-08-04 17:06:54 +0200

    RedisService ip

--- src/main/java/com/xavelo/piapi/service/RedisService.java
+++ src/main/java/com/xavelo/piapi/service/RedisService.java
@@ -4,19 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RedisService {
 
-    private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisService.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private RedisTemplate<Object, Object> redisTemplate;
 
     public void saveData(String key, Object data) {
-        logger.info("-> redis save key '" + key + "' with value '" + data  + "'");
+        String redisIP = env.getProperty("spring.data.redis.host");
+        LOGGER.info("-> redis save key '" + key + "' with value '" + data  + "' - " + redisIP);
         redisTemplate.opsForValue().set(key, data);
     }
 
