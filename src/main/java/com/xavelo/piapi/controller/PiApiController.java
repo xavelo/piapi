bd821515bd4c221aa3d7dad3ca1e3bb8f11df1e9
xavelo <xavelo@gmail.com>
2024-08-23 17:48:34 +0200

    cleanup

--- src/main/java/com/xavelo/piapi/controller/PiApiController.java
+++ src/main/java/com/xavelo/piapi/controller/PiApiController.java
@@ -1,8 +1,5 @@
 package com.xavelo.piapi.controller;
 
-import com.xavelo.piapi.service.KafkaService;
-import com.xavelo.piapi.service.LedService;
-import com.xavelo.piapi.service.MeteoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,43 +15,11 @@ public class PiApiController {
 
     private static final Logger logger = LoggerFactory.getLogger(PiApiController.class);
 
-    @Autowired
-    private KafkaService kafkaService;
-
-    @Autowired
-    private LedService ledService;
-
-    @Autowired
-    private MeteoService meteoService;
-
     @GetMapping("/ping")
     public String ping() {
         logger.info("ping received");
         logger.info("pong returned");
-        return "pooooooooooooooooooooooooooooooooooooooong";
+        return "POOONG";
     }
-
-    @PostMapping("/message")
-    public String sendMessage(@RequestBody Map<String, String> json) {
-        String topic = json.get("topic");
-        String message = json.get("message");        
-        kafkaService.sendMessage(topic, message);
-        return "Message sent to Kafka: " + message;
-    }
-
-    @PostMapping("/meteo/message")
-    public String meteoMessage(@RequestBody Map<String, String> json) {
-        String message = json.get("message");
-        meteoService.meteoActivity(message);
-        return "Message sent to Kafka: " + message;
-    }
-
-    @PostMapping("/led/message")
-    public String led(@RequestBody Map<String, String> json) {
-        String message = json.get("message");
-        ledService.ledActivity(message);
-        return "Led message sent to Kafka: " + message;
-    }
-
 }
 
